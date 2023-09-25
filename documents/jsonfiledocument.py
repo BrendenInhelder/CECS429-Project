fbcc8772b66041022de1f9892052aa742dbb03fd
BrendenInhelder <brendeninhelder@gmail.com>
2023-09-24 22:22:26 -0700

    json document compatibility (untested)

---
+++ documents/jsonfiledocument.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from typing import Iterable
+from document import Document
+import json
+
+class JsonFileDocument(Document):
+    """
+    Represents a document that is saved as a json file in the local file system.
+    """
+    def __init__(self, id : int, path : Path):
+        super().__init__(id)
+        self.path = path
+
+    @property
+    def title(self) -> str:
+        return self.path.stem
+
+    def get_content(self) -> Iterable[str]:
+        with open(self.path, 'r') as json_file:
+            json_data = json.load(json_file)
+            body_text = json_data.get("body", "")
+        return body_text
+
+    @staticmethod
+    def load_from(abs_path : Path, doc_id : int) -> 'JsonFileDocument' :
+        """A factory method to create a JsonFileDocument around the given file path."""
+        return JsonFileDocument(doc_id, abs_path)
