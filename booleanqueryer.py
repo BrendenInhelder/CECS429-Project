a588874326ef5b6441e377ff06e85919916c2630
BrendenInhelder <brendeninhelder@gmail.com>
2023-10-03 22:33:39 -0700

    Using intermediate token processor (w/out stemmer)

--- booleanqueryer.py
+++ booleanqueryer.py
@@ -3,12 +3,13 @@ import pprint
 from documents import DocumentCorpus, DirectoryCorpus
 from indexing import Index, PositionalInvertedIndex
 from indexing.postings import Posting
-from text import BasicTokenProcessor, englishtokenstream
+from text import BasicTokenProcessor, englishtokenstream, IntermediateTokenProcessor
 from querying import BooleanQueryParser
 
 
 def positional_inverted_index_corpus(corpus: DocumentCorpus) -> Index:
-    token_processor = BasicTokenProcessor()
+    token_processor = IntermediateTokenProcessor()
+    # token_processor = BasicTokenProcessor()
     positional_inverted_index = PositionalInvertedIndex()
 
     for doc in corpus:
@@ -17,9 +18,16 @@ def positional_inverted_index_corpus(corpus: DocumentCorpus) -> Index:
         token_stream = englishtokenstream.EnglishTokenStream(doc.get_content())
         for token in token_stream:
             term = token_processor.process_token(token) 
-            positional_inverted_index.add_term(term, doc.id, position)
-            positional_inverted_index.vocabulary.add(term)
-            position += 1
+            if type(term) is not list:
+                positional_inverted_index.add_term(term, doc.id, position)
+                positional_inverted_index.vocabulary.add(term)
+                position += 1
+            else:
+                for terms in term:
+                    positional_inverted_index.add_term(terms, doc.id, position)
+                    positional_inverted_index.vocabulary.add(terms)
+                    #TODO: how do we deal with positions of terms that technically don't exist because we created them from hyphens???
+                    position += 1
     return positional_inverted_index
 
 if __name__ == "__main__":
@@ -46,6 +54,7 @@ if __name__ == "__main__":
         result = queryComponent.get_postings(index)
         print("result: ")
         for docID in result:
+            # print(result) if you want to see the positions
             print("Title:", (d.get_document(Posting(docID).doc_id)).title, "ID:", docID)
         query = input('Enter a term you would like to search for(\'quit\' to exit): ')
 
