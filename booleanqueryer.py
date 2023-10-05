82d7f3093394f94913a7b4ce653aaaf5432f6d1b
BrendenInhelder <brendeninhelder@gmail.com>
2023-10-04 23:19:59 -0700

    Phrase queries, buggy user query processing

--- booleanqueryer.py
+++ booleanqueryer.py
@@ -19,26 +19,45 @@ def positional_inverted_index_corpus(corpus: DocumentCorpus) -> Index:
         for token in token_stream:
             term = token_processor.process_token(token) 
             if type(term) is not list:
-                term = token_processor.normalize_type(term)
+                # term = token_processor.normalize_type(term)
                 positional_inverted_index.add_term(term, doc.id, position)
                 positional_inverted_index.vocabulary.add(term)
                 position += 1
             else:
                 for terms in term:
-                    terms = token_processor.normalize_type(terms)
+                    # terms = token_processor.normalize_type(terms)
                     positional_inverted_index.add_term(terms, doc.id, position)
                     positional_inverted_index.vocabulary.add(terms)
                     #TODO: how do we deal with positions of terms that technically don't exist because we created them from hyphens???
                     position += 1
     return positional_inverted_index
 
+def get_query() -> str:
+    #BUG: results in porter stemmer on more than one term because query not separated
+    phraseQuery = False
+    query = input('Enter a term you would like to search for(\'quit\' to exit): ')
+    if query == 'quit':
+        return query
+    if query[0] == '\"'and query[-1] == '\"':
+        phraseQuery = True
+    token_processor = IntermediateTokenProcessor()
+    query = token_processor.process_token(query)
+    print("query as a list:", query)
+    if type(query) is list:
+        # not sure how to handle this yet...what if the user searches "co-education"? Do we search for all 3? (probably)
+        query = query[0]
+    print("query pre normal:", query)
+    query = token_processor.normalize_type(query)
+    if phraseQuery: # we need to retain the quotes for the boolean query parser to recognize as phrase query
+        query = '\"' + query + '\"'
+        phraseQuery = False
+    return query
+
 if __name__ == "__main__":
     # TODO: user input for path, hard-coded for debugging purposes
     # corpus_path = Path() 
-    # corpus_path = Path(r'C:\Users\Brend\OneDrive\Desktop\NPS10')
-    corpus_path = Path(r'C:\Users\Brend\Documents\all-nps-sites')
-    
-
+    corpus_path = Path(r'C:\Users\Brend\OneDrive\Desktop\NPS10')
+    # corpus_path = Path(r'C:\Users\Brend\Documents\all-nps-sites')
 
     # can use either txt or json...TODO: possibly try PDFs?
     # d = DirectoryCorpus.load_text_directory(corpus_path, ".txt")
@@ -48,18 +67,19 @@ if __name__ == "__main__":
     index = positional_inverted_index_corpus(d)
 
     query = input('Enter a term you would like to search for(\'quit\' to exit): ')
-    # TODO: apply same tokenization/normalization to query terms?
-
     while query != 'quit':
         queryComponent = BooleanQueryParser.parse_query(query)
         print("query:", queryComponent)
         result = queryComponent.get_postings(index)
-        print("result: ")
-        for docID in result:
-            # print(result) if you want to see the positions
-            print("Title:", (d.get_document(Posting(docID).doc_id)).title, "ID:", docID)
+        if len(result) == 0:
+            print("No results")
+        else:
+            print("result: ")
+            for docID in result:
+                # print(result) if you want to see the positions
+                print("Title:", (d.get_document(Posting(docID).doc_id)).title, "ID:", docID)
         query = input('Enter a term you would like to search for(\'quit\' to exit): ')
-
+    
     # Uncomment if you want the vocabulary printed
     # print('Vocab:')
     # print(index.get_vocabulary())
--- querying/phraseliteral.py
+++ querying/phraseliteral.py
@@ -1,3 +1,4 @@
+from indexing.index import Index
 from indexing.postings import Posting
 from .querycomponent import QueryComponent
 
@@ -10,9 +11,58 @@ class PhraseLiteral(QueryComponent):
         self.literals = terms
 
     def get_postings(self, index) -> list[Posting]:
-        return None
-        # TODO: program this method. Retrieve the postings for the individual literals in the phrase,
-		# and positional merge them together.
+        previousPostings = 0
+        docIDs = []
+        sharedIndexes = [] # holds the postings for each term
+        for component in self.literals:
+            sharedIndexes.append(index.get_postings(component.term))
+            docIDs = []
+            currentPostings = list((index.get_postings(component.term)).keys())
+            if previousPostings == 0:
+                previousPostings = currentPostings
+                continue
+            pprevious = 0
+            pcurrent = 0
+            while(pprevious < len(previousPostings) and pcurrent < len(currentPostings)):
+                previousDoc = previousPostings[pprevious]
+                currentDoc = currentPostings[pcurrent]
+                if previousDoc == currentDoc:
+                    # AND is true
+                    docIDs.append(previousDoc)
+                    pprevious += 1
+                    pcurrent += 1
+                elif previousDoc < currentDoc:
+                    pprevious += 1
+                else:
+                    pcurrent += 1
+            previousPostings = docIDs
+        if len(docIDs) == 0:
+            return docIDs
+        # time to perform the merge...yay
+        result = []
+        for docID in docIDs: # loop through each docID to check if it has a phrase
+            currentDocPositions = []
+            for termPositions in sharedIndexes: # look at each term's position index, check only the current docID
+                currentDocPositions.append(termPositions[docID])
+            if self.check_positions(currentDocPositions):
+                result.append(docID)
+        return result
+
+    def check_positions(self, currentDocPositions) -> bool:
+        # method to check each list of positions between terms for a phrase
+        k = 1
+        for position in currentDocPositions[0]:
+            found = False
+            while k < (len(currentDocPositions)):
+                if (position+k) in currentDocPositions[k]:
+                    found = True
+                    k += 1
+                else:
+                    found = False
+                    break
+            if found:
+                return True
+        return False
 
     def __str__(self) -> str:
         return '"' + " ".join(map(str, self.literals)) + '"'
