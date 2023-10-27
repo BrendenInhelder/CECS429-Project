a86be652b31bd1613b18f6c59533085213e236fa
BrendenInhelder <brendeninhelder@gmail.com>
2023-10-26 23:20:07 -0700

    vocab db, needs more testing

--- indexing/diskindexwriter.py
+++ indexing/diskindexwriter.py
@@ -1,17 +1,27 @@
 import os
 from pathlib import Path
+import sqlite3
 import struct
 from indexing import PositionalInvertedIndex
 
 class DiskIndexWriter():
     def writeIndex(self, index: PositionalInvertedIndex, savePath):
-        # writeIndex will (hopefully) write an index to "disk" to be read
-        # Leaving a lot of comments for now because it's confusing 
-        # TODO: vocabulary
-        bin_format = 'i'
+        # writeIndex will write an index to "disk" to be read
+        # TODO: vocabulary...might work properly?
+        # setup the db for the vocab
+        vocabConnection = sqlite3.connect("vocabulary.db")
+        cursor = vocabConnection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS vocab (term TEXT PRIMARY KEY, byte INTEGER)")
+        vocabConnection.commit()
 
+        bin_format = 'i'
         with open(savePath, 'wb') as index_on_disk_file:
             for term, postingsList in index.index.items():
+                # first we store the terms byte location
+                insert_structure = "INSERT INTO vocab (term, byte) VALUES (?, ?)"
+                cursor.execute(insert_structure, (term, index_on_disk_file.tell()))
+                vocabConnection.commit()
+
                 dft = len(postingsList) # document frequency for term
                 packed_data = struct.pack(bin_format, dft)
                 index_on_disk_file.write(packed_data)
